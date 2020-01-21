b132e87b749feac23b17cdd30e5c53d3f433eed4
Nikola Žitko <nikola.zitko@gmail.com>
2020-01-21 15:08:52 +0100

    tilde '~' points to home dir

--- main.c
+++ main.c
@@ -37,6 +37,41 @@ char *lsh_read_line(void){
 }
 
 
+char* tilde(char *orig){
+    char* rep = (char*)malloc(sizeof(char)*2);
+    rep = strcpy(rep, "~");
+    char* with = getenv("HOME");
+    char *result, *ins, *tmp;
+    int len_rep, len_with, len_front, count;
+
+    if (!orig || !rep)
+        return NULL;
+    len_rep = strlen(rep);
+    if(len_rep == 0)
+        return NULL;
+    if(!with)
+        with = "";
+    len_with = strlen(with);
+
+    ins = orig;
+    for (count = 0; tmp = strstr(ins, rep); ++count){
+        ins = tmp + len_rep;
+    }
+    
+    tmp = result = malloc(strlen(orig) + (len_with - len_rep) * count + 1);
+    if(!result)
+        return NULL;
+    
+    while(count--){
+        ins = strstr(orig, rep);
+        len_front = ins - orig;
+        tmp = strncpy(tmp, orig, len_front) + len_front;
+        tmp = strcpy(tmp, with) + len_with;
+        orig += len_front + len_rep;
+    }
+    strcpy(tmp, orig);
+    return result;
+}
 
 void lsh_loop(void){
 
@@ -49,6 +84,7 @@ void lsh_loop(void){
 		printf("%s@%s%s %s-->%s ",COLOR_BLUE, username, RESET, BOLD, RESET);
 		line = lsh_read_line();
         history(line);
+        line = tilde(line);
 		args = lsh_split_line(line);
 		status = lsh_execute(args);
 		free (line);
